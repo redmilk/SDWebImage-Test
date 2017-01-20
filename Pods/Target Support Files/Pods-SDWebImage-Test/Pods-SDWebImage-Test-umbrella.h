e02db581c72124aba66bc52b7fdb1a24e6afbee6
Artem <Artem@Danils-MacBook-Pro.local>
2017-01-20 11:54:16 +0200

    iTimofeev. SDWebImage podfile project

---
+++ Pods/Target Support Files/Pods-SDWebImage-Test/Pods-SDWebImage-Test-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SDWebImage_TestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SDWebImage_TestVersionString[];
+
